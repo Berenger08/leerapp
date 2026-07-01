1de879244eb08d4dfe69fdebfe5e6a902cb486e8
Martijn <martijn@famderuijter.com>
2026-07-01 18:47:04 +0200

    Visuele restyling: warm kleurenpalet, ronde kaarten, betere typografie en polish

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'leerapp-cache-v1';
+const CACHE_NAME = 'leerapp-cache-v2';
 const ASSETS = ['./', './index.html', './style.css', './script.js', './manifest.json'];
 
 self.addEventListener('install', event => {
